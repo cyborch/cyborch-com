f1988d66981462dcea629ddcec6d04ce179b95bd
Anders Borch <anders@cyborch.com>
2020-04-28 10:16:14 +0200

    Added helm chart

---
+++ scripts/deploy.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+set -e
+
+cd "$(dirname "$0")/.."
+
+docker build -t cyborch/cyborch-com:latest .
+docker-squash cyborch/cyborch-com -t cyborch/cyborch-com:latest
+docker push cyborch/cyborch-com:latest
+helm install cyborch-com ./helm | helm upgrade cyborch-com ./helm
+echo ""
+echo 👑 successfully deployed!
