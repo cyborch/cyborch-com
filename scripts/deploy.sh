a3ff5d9619f6e0a356405c7540e17f4b09f75403
Anders Borch <anders@cyborch.com>
2020-04-28 11:01:45 +0200

    Fixed typo

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,6 +7,6 @@ cd "$(dirname "$0")/.."
 docker build -t cyborch/cyborch-com:latest .
 docker-squash cyborch/cyborch-com -t cyborch/cyborch-com:latest
 docker push cyborch/cyborch-com:latest
-helm install cyborch-com ./helm | helm upgrade cyborch-com ./helm
+helm install cyborch-com ./helm || helm upgrade cyborch-com ./helm
 echo ""
 echo 👑 successfully deployed!
