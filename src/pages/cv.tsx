491976b4b722189e6b41760d2e952e7baa9bdcba
Anders Borch <anders@cyborch.com>
2022-11-03 08:46:51 +0100

    Removed text when redirecting to pdf

--- src/pages/cv.tsx
+++ src/pages/cv.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import Layout from '../components/layout';
 import { Helmet } from 'react-helmet';
-import './cv.styl';
 
 export default function Index() {
   return (
@@ -12,12 +11,7 @@ export default function Index() {
             content={`0;url=/cv.pdf`}
         />
       </Helmet>
-      <div id="cv">
-        <p>
-          Download should begin immediately, if not, then 
-          press <a href="/cv.pdf" download>here</a>.
-        </p>
-      </div>
+      <p></p>
     </Layout>
   )
 }
